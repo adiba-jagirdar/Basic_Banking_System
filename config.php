fb717148aa9ab21ef63f2a69cd8acbc0fc66e57f
Adiba Jagirdar <77891122+adiba-jagirdar@users.noreply.github.com>
2021-02-03 14:23:22 +0530

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	$conn = mysqli_connect('localhost','id16065385_repent','Database@123','id16065385_adiba');
-
-	if(!$conn){
-		die("Could not connect to the database due to the following error --> ".mysqli_connect_error());
-	}
-
-?>
